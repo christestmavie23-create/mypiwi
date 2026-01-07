bb90e9d4f56dbf2a6b2cb1e242f665690d8d5968
Wilfried Ndouga <christestmavie23@gmail.com>
2026-01-07 09:16:34 +0000

    Initial-commit Piwi Alarm PWA

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'piwi-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+// Installation du Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activation et nettoyage des anciens caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retourne la version en cache ou fait une nouvelle requête
+        return response || fetch(event.request);
+      })
+  );
+});
